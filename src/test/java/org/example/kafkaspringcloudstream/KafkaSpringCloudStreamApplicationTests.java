85f57a872f75086e3262d38af0a568d083fe44af
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-09-30 15:54:24 +0100

    pageevent commit

---
+++ src/test/java/org/example/kafkaspringcloudstream/KafkaSpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kafkaspringcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringCloudStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
